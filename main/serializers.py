e980b10ade0a5f5aff9757d7707b6ff7c8612c76
mc <mohcench9@gmail.com>
2023-12-16 11:02:57 +0100

    make user and doctor and patient serializers work properly with the api endpoint

--- main/serializers.py
+++ main/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import *
-from django.db.models import fields
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password as val_password
 
 
 class QuestionSerializer(serializers.ModelSerializer):
@@ -23,13 +24,51 @@ class QuestionnaireSerializer(serializers.ModelSerializer):
         model = Questionnaire
         fields = '__all__'
 
-class PatientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Patient
-        fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        exclude = ["is_active","is_staff","is_superuser"]
+
+    def create(self, validated_data):
+        validated_data["password"] = make_password(validated_data.get("password"))
+        user = super(UserSerializer, self).create(validated_data)
+        return user
+    
+    def validate_password(self, value):
+        val_password(value, self.instance)
+        return value
+
+class PatientSerializer(serializers.ModelSerializer):
+
+    user = UserSerializer()
+    class Meta:
+        model = Patient
         fields = '__all__'
 
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user_serializer = UserSerializer(data=user_data)
+        if user_serializer.is_valid():
+            user = user_serializer.save()
+            patient = Patient.objects.create(user=user, **validated_data)
+            return patient
+        else:
+            raise serializers.ValidationError(user_serializer.errors)
+class DoctorSerializer(serializers.ModelSerializer):
+
+    user = UserSerializer()
+    class Meta:
+        model = Doctor
+        fields = '__all__'
+        
+    
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user_serializer = UserSerializer(data=user_data)
+        if user_serializer.is_valid():
+            user = user_serializer.save()
+            doctor = Doctor.objects.create(user=user, **validated_data)
+            return doctor
+        else:
+            raise serializers.ValidationError(user_serializer.errors)
